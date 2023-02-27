fe66bd20c83b1e0bc587d3893a0a2f6153c01bac
liushuliang <shuliang.liu@capstones.cn>
2023-02-27 12:01:48 +0800

    修改配置

--- atc_template.py
+++ atc_template.py
@@ -20,29 +20,7 @@ RILST = lambda: list(RI())
 DEBUG = lambda *x: sys.stderr.write(f'{str(x)}\n')
 
 MOD = 10 ** 9 + 7
-PROBLEM = """https://atcoder.jp/contests/abc214/tasks/abc214_e
-
-输入 t(≤2e5) 表示 t 组数据，每组数据输入 n(≤2e5) 和 n 个区间 [L,R]，范围在 [1,1e9]。
-所有数据的 n 之和不超过 2e5。
-
-你有 n 个球，第 i 个球需要放在区间 [L,R] 内的整数位置上。
-但每个整数位置上至多能放一个球。
-如果可以做到，输出 Yes，否则输出 No
-输入
-2
-3
-1 2
-2 3
-3 3
-5
-1 2
-2 3
-3 3
-1 3
-999999999 1000000000
-输出
-Yes
-No
+PROBLEM = """
 """
 
 
