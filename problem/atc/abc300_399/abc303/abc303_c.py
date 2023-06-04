e8e3ea4d156b22f0f2ace5e6b32e0f1a450ed624
qishuih <liuliang.can@163.com>
2023-06-04 09:55:08 +0800

    abc303

---
+++ problem/atc/abc300_399/abc303/abc303_c.py
@@ -0,0 +1,78 @@
+# Problem: C - Dash
+# Contest: AtCoder - NS Solutions Corporation Programming Contest 2023（AtCoder Beginner Contest 303）
+# URL: https://atcoder.jp/contests/abc303/tasks/abc303_c
+# Memory Limit: 1024 MB
+# Time Limit: 2000 ms
+
+import sys
+import bisect
+import random
+import io, os
+from bisect import *
+from collections import *
+from contextlib import redirect_stdout
+from itertools import *
+from array import *
+from functools import lru_cache, reduce
+from types import GeneratorType
+from heapq import *
+from math import sqrt, gcd, inf
+
+if sys.version >= '3.8':  # ACW没有comb
+    from math import comb
+
+RI = lambda: map(int, sys.stdin.buffer.readline().split())
+RS = lambda: map(bytes.decode, sys.stdin.buffer.readline().strip().split())
+RILST = lambda: list(RI())
+DEBUG = lambda *x: sys.stderr.write(f'{str(x)}\n')
+# print = lambda d: sys.stdout.write(str(d) + "\n")  # 打开可以快写，但是无法使用print(*ans,sep=' ')这种语法,需要print(' '.join(map(str, p)))，确实会快。
+
+DIRS = [(0, 1), (1, 0), (0, -1), (-1, 0)]  # 右下左上
+DIRS8 = [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0),
+         (-1, 1)]  # →↘↓↙←↖↑↗
+
+MOD = 10**9 + 7
+PROBLEM = """
+"""
+
+
+#       ms
+def solve():
+    n,m,h,k = RI()
+    s, = RS()
+    ps = set()
+    for _ in range(m):
+        x,y = RI()
+        ps.add((x,y))
+    x=y=0
+    for c in s:
+        if c == 'R':
+            x += 1
+        elif c =='L':
+            x -= 1
+        elif c == 'U':
+            y += 1
+        else :
+            y -= 1
+
+        h -= 1
+        if h<0:
+            return print('No')
+        if h<k and (x,y) in ps:
+            h = k
+            ps.remove((x,y))
+    print('Yes')
+
+
+
+
+
+
+if __name__ == '__main__':
+    t = 0
+    if t:
+        t, = RI()
+        for _ in range(t):
+            solve()
+    else:
+        solve()
---
+++ problem/atc/abc300_399/abc303/abc303_d.py
@@ -0,0 +1,103 @@
+# Problem: D - Shift vs. CapsLock
+# Contest: AtCoder - NS Solutions Corporation Programming Contest 2023（AtCoder Beginner Contest 303）
+# URL: https://atcoder.jp/contests/abc303/tasks/abc303_d
+# Memory Limit: 1024 MB
+# Time Limit: 2000 ms
+
+import sys
+import bisect
+import random
+import io, os
+from bisect import *
+from collections import *
+from contextlib import redirect_stdout
+from itertools import *
+from array import *
+from functools import lru_cache, reduce
+from types import GeneratorType
+from heapq import *
+from math import sqrt, gcd, inf
+
+if sys.version >= '3.8':  # ACW没有comb
+    from math import comb
+
+RI = lambda: map(int, sys.stdin.buffer.readline().split())
+RS = lambda: map(bytes.decode, sys.stdin.buffer.readline().strip().split())
+RILST = lambda: list(RI())
+DEBUG = lambda *x: sys.stderr.write(f'{str(x)}\n')
+# print = lambda d: sys.stdout.write(str(d) + "\n")  # 打开可以快写，但是无法使用print(*ans,sep=' ')这种语法,需要print(' '.join(map(str, p)))，确实会快。
+
+DIRS = [(0, 1), (1, 0), (0, -1), (-1, 0)]  # 右下左上
+DIRS8 = [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0),
+         (-1, 1)]  # →↘↓↙←↖↑↗
+
+MOD = 10 ** 9 + 7
+PROBLEM = """
+"""
+
+
+def lower_bound(lo: int, hi: int, key):
+    """由于3.10才能用key参数，因此自己实现一个。
+    :param lo: 二分的左边界(闭区间)
+    :param hi: 二分的右边界(闭区间)
+    :param key: key(mid)判断当前枚举的mid是否应该划分到右半部分。
+    :return: 右半部分第一个位置。若不存在True则返回hi+1。
+    虽然实现是开区间写法，但为了思考简单，接口以[左闭,右闭]方式放出。
+    """
+    lo -= 1  # 开区间(lo,hi)
+    hi += 1
+    while lo + 1 < hi:  # 区间不为空
+        mid = (lo + hi) >> 1  # py不担心溢出，实测py自己不会优化除2，手动写右移
+        if key(mid):  # is_right则右边界向里移动，目标区间剩余(lo,mid)
+            hi = mid
+        else:  # is_left则左边界向里移动，剩余(mid,hi)
+            lo = mid
+    return hi
+
+
+def bootstrap(f, stack=[]):
+    def wrappedfunc(*args, **kwargs):
+        if stack:
+            return f(*args, **kwargs)
+        else:
+            to = f(*args, **kwargs)
+            while True:
+                if type(to) is GeneratorType:
+                    stack.append(to)
+                    to = next(to)
+                else:
+                    stack.pop()
+                    if not stack:
+                        break
+                    to = stack[-1].send(to)
+            return to
+
+    return wrappedfunc
+
+
+#       ms
+def solve():
+    x, y, z = RI()
+    s, = RS()
+    n = len(s)
+    f = [[inf] * 2 for _ in range(n + 1)]
+    f[0] = [0, z]
+    for i, c in enumerate(s):
+        if c == 'a':
+            f[i + 1][0] = min(f[i][0] + x, f[i][1] + z + x)
+            f[i + 1][1] = min(f[i][0] + z + y , f[i][1] + y )
+        else:
+            f[i + 1][0] = min(f[i][0] + y , f[i][1] + z + y )
+            f[i + 1][1] = min(f[i][0] + z + x, f[i][1] + x)
+    # print(f)
+    print(min(f[-1]))
+
+
+if __name__ == '__main__':
+    t = 0
+    if t:
+        t, = RI()
+        for _ in range(t):
+            solve()
+    else:
+        solve()
