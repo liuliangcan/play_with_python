5dd351a46dd87cf856aeb029105980263cdd80dc
qishuih <liuliang.can@163.com>
2023-07-02 20:47:33 +0800

    牛客周赛1 模拟/分组/贪心/同余dp

---
+++ problem/nowcoder/w1/D.py
@@ -0,0 +1,70 @@
+# Problem: 游游的9的倍数
+# Contest: NowCoder
+# URL: https://ac.nowcoder.com/acm/contest/60245/D
+# Memory Limit: 524288 MB
+# Time Limit: 2000 ms
+
+import sys
+import random
+from types import GeneratorType
+import bisect
+import io, os
+from bisect import *
+from collections import *
+from contextlib import redirect_stdout
+from itertools import *
+from array import *
+from functools import lru_cache, reduce
+from heapq import *
+from math import sqrt, gcd, inf
+
+if sys.version >= '3.8':  # ACW没有comb
+    from math import comb
+
+RI = lambda: map(int, sys.stdin.buffer.readline().split())
+RS = lambda: map(bytes.decode, sys.stdin.buffer.readline().strip().split())
+RILST = lambda: list(RI())
+DEBUG = lambda *x: sys.stderr.write(f'{str(x)}\n')
+# print = lambda d: sys.stdout.write(str(d) + "\n")  # 打开可以快写，但是无法使用print(*ans,sep=' ')这种语法,需要print(' '.join(map(str, p)))，确实会快。
+
+DIRS = [(0, 1), (1, 0), (0, -1), (-1, 0)]  # 右下左上
+DIRS8 = [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0),
+         (-1, 1)]  # →↘↓↙←↖↑↗
+RANDOM = random.randrange(2 ** 62)
+MOD = 10 ** 9 + 7
+# MOD = 998244353
+PROBLEM = """
+"""
+
+
+#       ms
+def solve():
+    s, = RS()
+    s = list(map(int, s))
+    cnt = [0] * 9
+    ans = 0
+    for v in s:
+        f = [0] * 9
+        for j, c in enumerate(cnt):
+            p = (j * 10 + v) % 9
+            if p == 0:
+                ans = (ans + c) % MOD
+            f[p] += c
+        if v % 9 == 0:
+            ans = (ans + 1) % MOD
+
+        f[v % 9] += 1
+        for i, v in enumerate(f):
+            cnt[i] = (cnt[i] + v) % MOD
+
+    print(ans)
+
+
+if __name__ == '__main__':
+    t = 0
+    if t:
+        t, = RI()
+        for _ in range(t):
+            solve()
+    else:
+        solve()
