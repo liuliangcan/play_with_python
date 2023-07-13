4c284e7175318dd4dfb23571a891b97233ad2679
liushuliang <shuliang.liu@capstones.cn>
2023-07-13 17:40:28 +0800

    位运算

---
+++ problem/nowcoder/11225/E.py
@@ -0,0 +1,53 @@
+# Problem: 或与异或
+# Contest: NowCoder
+# URL: https://ac.nowcoder.com/acm/contest/11225/E?&headNav=acm
+# Memory Limit: 524288 MB
+# Time Limit: 2000 ms
+
+import sys
+import random
+from types import GeneratorType
+import bisect
+import io, os
+from bisect import *
+from collections import *
+from contextlib import redirect_stdout
+from itertools import *
+from array import *
+from functools import lru_cache, reduce
+from heapq import *
+from math import sqrt, gcd, inf
+
+if sys.version >= '3.8':  # ACW没有comb
+    from math import comb
+
+RI = lambda: map(int, sys.stdin.buffer.readline().split())
+RS = lambda: map(bytes.decode, sys.stdin.buffer.readline().strip().split())
+RILST = lambda: list(RI())
+DEBUG = lambda *x: sys.stderr.write(f'{str(x)}\n')
+# print = lambda d: sys.stdout.write(str(d) + "\n")  # 打开可以快写，但是无法使用print(*ans,sep=' ')这种语法,需要print(' '.join(map(str, p)))，确实会快。
+
+DIRS = [(0, 1), (1, 0), (0, -1), (-1, 0)]  # 右下左上
+DIRS8 = [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0),
+         (-1, 1)]  # →↘↓↙←↖↑↗
+RANDOM = random.randrange(2 ** 62)
+MOD = 10 ** 9 + 7
+# MOD = 998244353
+PROBLEM = """
+"""
+
+
+#       ms
+def solve():
+    a, b, t = RI()
+    print(['NO', 'YES'][t in {a, b, a & b, a | b, a ^ b, 0, a ^ (a & b), b ^ (a & b)}])
+
+
+if __name__ == '__main__':
+    t = 1
+    if t:
+        t, = RI()
+        for _ in range(t):
+            solve()
+    else:
+        solve()
