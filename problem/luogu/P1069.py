3b1dfe16a5fbfcf53881a5004e429c0652354b6e
liushuliang <shuliang.liu@capstones.cn>
2023-08-23 16:26:44 +0800

    洛谷p1069 P1069 [NOIP2009 普及组] 细胞分裂

---
+++ problem/luogu/P1069.py
@@ -0,0 +1,160 @@
+import sys
+import random
+from types import GeneratorType
+import bisect
+import io, os
+from bisect import *
+from collections import *
+from contextlib import redirect_stdout
+from itertools import *
+from array import *
+from functools import lru_cache, reduce
+from heapq import *
+from math import sqrt, gcd, inf
+
+if sys.version >= '3.8':  # ACW没有comb
+    from math import comb
+
+RI = lambda: map(int, sys.stdin.buffer.readline().split())
+RS = lambda: map(bytes.decode, sys.stdin.buffer.readline().strip().split())
+RILST = lambda: list(RI())
+DEBUG = lambda *x: sys.stderr.write(f'{str(x)}\n')
+# print = lambda d: sys.stdout.write(str(d) + "\n")  # 打开可以快写，但是无法使用print(*ans,sep=' ')这种语法,需要print(' '.join(map(str, p)))，确实会快。
+
+DIRS = [(0, 1), (1, 0), (0, -1), (-1, 0)]  # 右下左上
+DIRS8 = [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0),
+         (-1, 1)]  # →↘↓↙←↖↑↗
+RANDOM = random.randrange(2 ** 62)
+MOD = 10 ** 9 + 7
+# MOD = 998244353
+PROBLEM = """https://www.luogu.com.cn/problem/P1069
+P1069 [NOIP2009 普及组] 细胞分裂
+"""
+
+
+class PrimeTable:
+    def __init__(self, n: int) -> None:
+        self.n = n
+        self.primes = primes = []  # 所有n以内的质数
+        self.min_div = min_div = [0] * (n + 1)  # md[i]代表i的最小(质)因子
+        min_div[1] = 1
+
+        # 欧拉筛O(n)，顺便求出min_div
+        for i in range(2, n + 1):
+            if not min_div[i]:
+                primes.append(i)
+                min_div[i] = i
+            for p in primes:
+                if i * p > n: break
+                min_div[i * p] = p
+                if i % p == 0:
+                    break
+
+    def is_prime(self, x: int):
+        """检测是否是质数，最坏是O(sqrt(x)"""
+        if x < 3: return x == 2
+        if x <= self.n: return self.min_div[x] == x
+        for i in range(2, int(x ** 0.5) + 1):
+            if x % i == 0: return False
+        return True
+
+    def prime_factorization(self, x: int):
+        """分解质因数，复杂度
+        1. 若x>n则需要从2模拟到sqrt(x)，如果中间x降到n以下则走2；最坏情况，不含低于n的因数，则需要开方复杂度
+        2. 否则x质因数的个数，那么最多就是O(lgx)"""
+        n, min_div = self.n, self.min_div
+        for p in range(2, int(x ** 0.5) + 1):
+            if x <= n: break
+            if x % p == 0:
+                cnt = 0
+                while x % p == 0: cnt += 1; x //= p
+                yield p, cnt
+        while 1 < x <= n:
+            p, cnt = min_div[x], 0
+            while x % p == 0: cnt += 1; x //= p
+            yield p, cnt
+        if x >= n and x > 1:
+            yield x, 1
+
+    def get_factors(self, x: int):
+        """求x的所有因数，包括1和x"""
+        factors = [1]
+        for p, b in self.prime_factorization(x):
+            n = len(factors)
+            for j in range(1, b + 1):
+                for d in factors[:n]:
+                    factors.append(d * (p ** j))
+        return factors
+
+    def mr_is_prime(self, x):
+        """
+        Miller-Rabin 检测. 检测x是否是质数，置信度: 1 - (1/4)^k. 复杂度k*log^3
+        但是longlong以内可以用k==3或7的代价，换取100%置信度
+        https://zhuanlan.zhihu.com/p/349360074
+        """
+        if x < 3 or x % 2 == 0:
+            return x == 2
+        if x % 3 == 0:
+            return x == 3
+
+        u, t = x - 1, 0
+        while not u & 1:
+            u >>= 1
+            t += 1
+        ud = (2, 325, 9375, 28178, 450775, 9780504, 1795265022)  # long long 返回用这个7个数检测100%正确
+        # ud = (2, 7, 61)  # int 返回用这3个数检测100%正确
+        # for _ in range(k):
+        #     a = random.randint(2, x - 2)
+        for a in ud:
+            v = pow(a, u, x)
+            if v == 1 or v == x - 1 or v == 0:
+                continue
+            for j in range(1, t + 1):
+                v = v * v % x
+                if v == x - 1 and j != t:
+                    v = 1
+                    break
+                if v == 1:
+                    return False
+            if v != 1:
+                return False
+        return True
+
+
+pt = PrimeTable(40000)
+
+
+#       ms
+def solve():
+    n, = RI()
+    m1, m2 = RI()
+    a = RILST()
+    cnt = [(p, v * m2) for p, v in pt.prime_factorization(m1)]
+
+    if m1 == 1:
+        return print(0)
+    ans = inf
+    for s in a:
+        p = 0
+        for k, v in cnt:
+            c = 0
+            while s % k == 0:
+                s //= k
+                c += 1
+            if c == 0:
+                break
+            p = max(p, (v + c - 1) // c)
+        else:
+            ans = min(ans, p)
+
+    print([ans, -1][ans == inf])
+
+
+if __name__ == '__main__':
+    t = 0
+    if t:
+        t, = RI()
+        for _ in range(t):
+            solve()
+    else:
+        solve()
