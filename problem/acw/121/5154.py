b274c9f28848f676762894f0144043f84738a25f
qishuih <liuliang.can@163.com>
2023-09-16 19:17:26 +0800

    acw121 模拟

---
+++ problem/acw/121/5154.py
@@ -0,0 +1,64 @@
+# Problem: 程序调用
+# Contest: AcWing
+# URL: https://www.acwing.com/problem/content/5154/
+# Memory Limit: 256 MB
+# Time Limit: 1000 ms
+
+import sys
+import random
+from types import GeneratorType
+import bisect
+import io, os
+from bisect import *
+from collections import *
+from contextlib import redirect_stdout
+from itertools import *
+from array import *
+from functools import lru_cache, reduce
+from heapq import *
+from math import sqrt, gcd, inf
+
+if sys.version >= '3.8':  # ACW没有comb
+    from math import comb
+
+RI = lambda: map(int, sys.stdin.buffer.readline().split())
+RS = lambda: map(bytes.decode, sys.stdin.buffer.readline().strip().split())
+RILST = lambda: list(RI())
+DEBUG = lambda *x: sys.stderr.write(f'{str(x)}\n')
+# print = lambda d: sys.stdout.write(str(d) + "\n")  # 打开可以快写，但是无法使用print(*ans,sep=' ')这种语法,需要print(' '.join(map(str, p)))，确实会快。
+
+DIRS = [(0, 1), (1, 0), (0, -1), (-1, 0)]  # 右下左上
+DIRS8 = [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0),
+         (-1, 1)]  # →↘↓↙←↖↑↗
+RANDOM = random.randrange(2 ** 62)
+MOD = 10 ** 9 + 7
+# MOD = 998244353
+PROBLEM = """
+"""
+
+
+#       ms
+def solve():
+    n, m, k = RI()
+    a = RILST()
+    b = RILST()
+    pos = {v: i for i, v in enumerate(a)}
+    ans = 0
+    for v in b:
+        p = pos[v]
+        ans += p // k + 1
+        if p == 0: continue
+        x = a[p - 1]
+        pos[x], pos[v] = p, p - 1
+        a[p - 1], a[p] = v, x
+    print(ans)
+
+
+if __name__ == '__main__':
+    t = 0
+    if t:
+        t, = RI()
+        for _ in range(t):
+            solve()
+    else:
+        solve()
