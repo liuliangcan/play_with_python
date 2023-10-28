9381ccec2eb3d296abffbd916ca3b81ad1e18567
qishuih <liuliang.can@163.com>
2023-10-28 22:04:49 +0800

    abc326 滑窗/回溯/期望DP

---
+++ problem/atc/abc300_399/abc326/abc326_b.py
@@ -0,0 +1,96 @@
+# Problem: B - 326-like Numbers
+# Contest: AtCoder - Panasonic Programming Contest 2023（AtCoder Beginner Contest 326）
+# URL: https://atcoder.jp/contests/abc326/tasks/abc326_b
+# Memory Limit: 1024 MB
+# Time Limit: 2000 ms
+
+import sys
+import random
+from types import GeneratorType
+import bisect
+import io, os
+from bisect import *
+from collections import *
+from contextlib import redirect_stdout
+from itertools import *
+from array import *
+from functools import lru_cache, reduce
+from heapq import *
+from math import sqrt, gcd, inf
+
+if not sys.version.startswith('3.5.3'):  # ACW没有comb
+    from math import comb
+
+RI = lambda: map(int, sys.stdin.buffer.readline().split())
+RS = lambda: map(bytes.decode, sys.stdin.buffer.readline().strip().split())
+RILST = lambda: list(RI())
+DEBUG = lambda *x: sys.stderr.write(f'{str(x)}\n')
+# print = lambda d: sys.stdout.write(str(d) + "\n")  # 打开可以快写，但是无法使用print(*ans,sep=' ')这种语法,需要print(' '.join(map(str, p)))，确实会快。
+
+DIRS = [(0, 1), (1, 0), (0, -1), (-1, 0)]  # 右下左上
+DIRS8 = [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0),
+         (-1, 1)]  # →↘↓↙←↖↑↗
+RANDOM = random.randrange(2 ** 62)
+MOD = 10 ** 9 + 7
+# MOD = 998244353
+PROBLEM = """
+"""
+
+
+def lower_bound(lo: int, hi: int, key):
+    """由于3.10才能用key参数，因此自己实现一个。
+    :param lo: 二分的左边界(闭区间)
+    :param hi: 二分的右边界(闭区间)
+    :param key: key(mid)判断当前枚举的mid是否应该划分到右半部分。
+    :return: 右半部分第一个位置。若不存在True则返回hi+1。
+    虽然实现是开区间写法，但为了思考简单，接口以[左闭,右闭]方式放出。
+    """
+    lo -= 1  # 开区间(lo,hi)
+    hi += 1
+    while lo + 1 < hi:  # 区间不为空
+        mid = (lo + hi) >> 1  # py不担心溢出，实测py自己不会优化除2，手动写右移
+        if key(mid):  # is_right则右边界向里移动，目标区间剩余(lo,mid)
+            hi = mid
+        else:  # is_left则左边界向里移动，剩余(mid,hi)
+            lo = mid
+    return hi
+
+
+def bootstrap(f, stack=[]):
+    def wrappedfunc(*args, **kwargs):
+        if stack:
+            return f(*args, **kwargs)
+        else:
+            to = f(*args, **kwargs)
+            while True:
+                if type(to) is GeneratorType:
+                    stack.append(to)
+                    to = next(to)
+                else:
+                    stack.pop()
+                    if not stack:
+                        break
+                    to = stack[-1].send(to)
+            return to
+
+    return wrappedfunc
+
+
+#       ms
+def solve():
+    n, = RI()
+    for i in range(n, 1000):
+        s = str(i)
+
+        if int(s[0]) * int(s[1]) == int(s[2]):
+            return print(i)
+
+
+if __name__ == '__main__':
+    t = 0
+    if t:
+        t, = RI()
+        for _ in range(t):
+            solve()
+    else:
+        solve()
