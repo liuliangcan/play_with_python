d4639c4ba8e98b6b88a9f228514627fdb2b430d2
liushuliang <liushuliang6@huawei.com>
2024-03-18 09:58:38 +0800

    中位数贪心

--- template/MedianGreedyDistance.py
+++ template/MedianGreedyDistance.py
@@ -6,18 +6,29 @@
 from itertools import accumulate
 
 
-class RangeDistance:
+class MedianGreedyDistance:
+    """计算升序数组里，所有值调整到同一个值的步数（距离）"""
+
     def __init__(self, a):
         self.pre = [0] + list(accumulate(a))
         self.a = a
 
-    def sum_dis(self, l, r, i):
+    def sum_dis_to_i(self, l, r, i):
         """计算a[l,r]里所有数字到a[i]的距离和"""
-
-        if i < l:
+        if i <= l:
             return self.pre[r + 1] - self.pre[l] - self.a[i] * (r - l + 1)
-        elif i > r:
+        elif i >= r:
             return self.a[i] * (r - l + 1) - self.pre[r + 1] + self.pre[l]
         else:
             # return self.a[i] * (i - l + 1) - self.pre[i + 1] + self.pre[l] + self.pre[r + 1] - self.pre[i] - self.a[i] * (r - i + 1)
             return self.a[i] * (i * 2 - l + 1 - r) - 2 * self.pre[i + 1] + self.pre[l] + self.pre[r + 1]
+
+    def sum_dis_to_v(self, l, r, v):
+        """计算a[l,r]里所有数字到v的距离和"""
+        if v <= self.a[l]:
+            return self.pre[r + 1] - self.pre[l] - v * (r - l + 1)
+        elif v >= self.a[r]:
+            return v * (r - l + 1) - self.pre[r + 1] + self.pre[l]
+        else:
+            i = bisect_left(self.a, v)
+            return v * (i * 2 - l + 1 - r) - 2 * self.pre[i + 1] + self.pre[l] + self.pre[r + 1]
