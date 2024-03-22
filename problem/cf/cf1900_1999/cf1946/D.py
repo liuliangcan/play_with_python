e38eae98f9f4880fd1a7da9af26a9ab7a883509a
liushuliang <512156619@qq.com>
2024-03-23 00:45:48 +0800

    cf1946 二分/并查集贪心

---
+++ problem/cf/cf1900_1999/cf1946/D.py
@@ -0,0 +1,162 @@
+# Problem: D. Birthday Gift
+# Contest: Codeforces - Codeforces Round 936 (Div. 2)
+# URL: https://codeforces.com/contest/1946/problem/D
+# Memory Limit: 256 MB
+# Time Limit: 2000 ms
+
+import sys
+import random
+from operator import ixor
+from types import GeneratorType
+import bisect
+import io, os
+from bisect import *
+from collections import *
+from contextlib import redirect_stdout
+from itertools import *
+from array import *
+from functools import lru_cache, reduce
+from heapq import *
+from math import sqrt, gcd, inf
+
+# if not sys.version.startswith('3.5.3'):  # ACW没有comb
+#     from math import comb
+
+RI = lambda: map(int, sys.stdin.buffer.readline().split())
+RS = lambda: map(bytes.decode, sys.stdin.buffer.readline().strip().split())
+RILST = lambda: list(RI())
+DEBUG = lambda *x: sys.stderr.write(f'{str(x)}\n')
+# print = lambda d: sys.stdout.write(str(d) + "\n")  # 打开可以快写，但是无法使用print(*ans,sep=' ')这种语法,需要print(' '.join(map(str, p)))，确实会快。
+
+DIRS = [(0, 1), (1, 0), (0, -1), (-1, 0)]  # 右下左上
+DIRS8 = [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1)]  # →↘↓↙←↖↑↗
+RANDOM = random.randrange(2 ** 62)
+MOD = 10 ** 9 + 7
+# MOD = 998244353
+PROBLEM = """
+"""
+
+
+def iii():  # 牛客输入格式有bug
+    num = 0
+    neg = False
+    while True:
+        c = sys.stdin.read(1)
+        if c == '-':
+            neg = True
+            continue
+        elif c < '0' or c > '9':
+            continue
+        while True:
+            num = 10 * num + ord(c) - ord('0')
+            c = sys.stdin.read(1)
+            if c < '0' or c > '9':
+                break
+        return -num if neg else num
+
+
+def lower_bound(lo: int, hi: int, key):
+    """由于3.10才能用key参数，因此自己实现一个。
+    :param lo: 二分的左边界(闭区间)
+    :param hi: 二分的右边界(闭区间)
+    :param key: key(mid)判断当前枚举的mid是否应该划分到右半部分。
+    :return: 右半部分第一个位置。若不存在True则返回hi+1。
+    虽然实现是开区间写法，但为了思考简单，接口以[左闭,右闭]方式放出。
+    """
+    lo -= 1  # 开区间(lo,hi)
+    hi += 1
+    while lo + 1 < hi:  # 区间不为空
+        mid = (lo + hi) >> 1  # py不担心溢出，实测py自己不会优化除2，手动写右移
+        if key(mid):  # is_right则右边界向里移动，目标区间剩余(lo,mid)
+            hi = mid
+        else:  # is_left则左边界向里移动，剩余(mid,hi)
+            lo = mid
+    return hi
+
+
+def bootstrap(f, stack=[]):
+    def wrappedfunc(*args, **kwargs):
+        if stack:
+            return f(*args, **kwargs)
+        else:
+            to = f(*args, **kwargs)
+            while True:
+                if type(to) is GeneratorType:
+                    stack.append(to)
+                    to = next(to)
+                else:
+                    stack.pop()
+                    if not stack:
+                        break
+                    to = stack[-1].send(to)
+            return to
+
+    return wrappedfunc
+
+
+#       ms
+def solve():
+    n, x = RI()
+    a = RILST()
+    if reduce(ixor, a) > x:
+        return print(-1)
+    ans = 1
+    fa = list(range(n))
+    cnt = n
+    def find(x, fa):
+        t = x
+        while fa[x] != x:
+            x = fa[x]
+        while t != x:
+            fa[t], t = x, fa[t]
+        return x
+    def union(x, y, fa):
+        x, y = find(x, fa), find(y, fa)
+        if x == y:
+            return False
+        fa[x] = y
+        return True
+
+    for j in range(29, -1, -1):
+        p = x >> j & 1
+        s = 0
+        if p == 0:
+            l = -1
+            for i, v in enumerate(a):
+                if l != -1:
+                    if union(l, i, fa):
+                        cnt -= 1
+                s ^= (v >> j & 1)
+                if s == 0:
+                    l = -1
+                else:
+                    l = i
+            if s != 0:
+                cnt = -1
+                break
+        else:
+            fa1 = fa[:]
+            l = -1
+            cnt1 = cnt
+            for i, v in enumerate(a):
+                if l != -1:
+                    if union(l, i, fa1):
+                        cnt1 -= 1
+                s ^= v >> j & 1
+                if s == 0:
+                    l = -1
+                else:
+                    l = i
+            if s == 0:
+                ans = max(ans, cnt1)
+    print(max(ans, cnt))
+
+
+if __name__ == '__main__':
+    t = 1
+    if t:
+        t, = RI()
+        for _ in range(t):
+            solve()
+    else:
+        solve()
