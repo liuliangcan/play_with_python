d97d62062551d0c99855cfeaad71dd06a4345d4e
liushuliang <512156619@qq.com>
2024-06-02 23:44:20 +0800

    改zkwlazy

--- problem/acw/107/5040.py
+++ problem/acw/107/5040.py
@@ -1,125 +1,256 @@
 # Problem: 区间异或
 # Contest: AcWing
-# URL: https://www.acwing.com/problem/content/5040/
+# URL: https://www.acwing.com/problem/content/description/5040/
 # Memory Limit: 256 MB
 # Time Limit: 2000 ms
 
 import sys
-
 RI = lambda: map(int, sys.stdin.buffer.readline().split())
 RS = lambda: map(bytes.decode, sys.stdin.buffer.readline().strip().split())
 RILST = lambda: list(RI())
 DEBUG = lambda *x: sys.stderr.write(f'{str(x)}\n')
-print = lambda d: sys.stdout.write(
-    str(d) + "\n")  # 打开可以快写，但是无法使用print(*ans,sep=' ')这种语法,需要print(' '.join(map(str, p)))，确实会快。
+print = lambda d: sys.stdout.write(str(d) + "\n")  # 打开可以快写，但是无法使用print(*ans,sep=' ')这种语法,需要print(' '.join(map(str, p)))，确实会快。
 
-MOD = 10 ** 9 + 7
-PROBLEM = """
-"""
+DIRS = [(0, 1), (1, 0), (0, -1), (-1, 0)]  # 右下左上
+DIRS8 = [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1)]  # →↘↓↙←↖↑↗
+
+import typing
 
 
-class IntervalTree:
-    def __init__(self, size, nums):
-        self.size = size
-        self.interval_tree = [0 for _ in range(size * 4)]
-        self.lazys = [0 for _ in range(size * 4)]
+class ZKWLazy:
+    def __init__(
+            self,
+            op: typing.Callable[[typing.Any, typing.Any], typing.Any],
+            e: typing.Any,
+            mapping: typing.Callable[[typing.Any, typing.Any], typing.Any],
+            composition: typing.Callable[[typing.Any, typing.Any], typing.Any],
+            id_: typing.Any,
+            v: typing.Union[int, typing.List[typing.Any]]) -> None:
+        self._op = op  # 两段的合并，一般是+，max之类
+        self._e = e  # 初始值e，一般要求任意元素x和e操作后返回值是e,例如max(x,e)=x,那么e=-inf;sum(x,e)=x,则e=0
+        self._mapping = mapping  # mapping(f,x) 代表x应用f操作后的值；f:修改时进行的操作；如果是assign操作，可以lambda f,x: f if f is not None else x ,注意f是后来的，会覆盖
+        self._composition = composition  # composition(f,g)两个tag的合并,即f(g(x))；如果是assign操作，可以llambda f,g: f if f is not None else g,注意f是后来的，会覆盖
+        self._id = id_  # lazy默认值，一般要求f(x)=x，及应用后x不变；那种0不行的，可以设置lazy=None(或其他无效值)，然后特判
 
-        self.nums = nums
-        self.build_tree(1, 1, size)
+        if isinstance(v, int):
+            v = [e] * v
 
-    def give_lay_to_son(self, p, l, r):
-        interval_tree = self.interval_tree
-        lazys = self.lazys
-        if lazys[p] == 0:
-            return
-        mid = (l + r) // 2
-        interval_tree[p * 2] = mid - l + 1 - interval_tree[p * 2]
-        interval_tree[p * 2 + 1] = r - mid - 1 + 1 - interval_tree[p * 2 + 1]
-        lazys[p * 2] ^= lazys[p]
-        lazys[p * 2 + 1] ^= lazys[p]
-        lazys[p] = 0
+        self._n = len(v)
+        self._log = (self._n - 1).bit_length()
+        self._size = 1 << self._log
+        self._d = [e] * (2 * self._size)
+        self._lz = [self._id] * self._size
+        for i in range(self._n):
+            self._d[self._size + i] = v[i]
+        for i in range(self._size - 1, 0, -1):
+            self._update(i)
 
-    def update(self, p, l, r, x, y, val):
-        """
-        把[x,y]区域全异或val
-        """
-        if y < l or r < x:
-            return
-        interval_tree = self.interval_tree
-        lazys = self.lazys
-        if x <= l and r <= y:
-            interval_tree[p] = (r-l+1)-interval_tree[p]
-            lazys[p] ^= val
-            return
-        self.give_lay_to_son(p, l, r)
-        mid = (l + r) // 2
-        if x <= mid:
-            self.update(p * 2, l, mid, x, y, val)
-        if mid < y:
-            self.update(p * 2 + 1, mid + 1, r, x, y, val)
-        interval_tree[p] = interval_tree[p * 2] + interval_tree[p * 2 + 1]
+    def set(self, p: int, x: typing.Any) -> None:
+        assert 0 <= p < self._n
 
-    def query(self, p, l, r, x, y):
-        """
-        查找x,y区间的和        """
+        p += self._size
+        for i in range(self._log, 0, -1):
+            self._push(p >> i)
+        self._d[p] = x
+        for i in range(1, self._log + 1):
+            self._update(p >> i)
 
-        if y < l or r < x:
+    def get(self, p: int) -> typing.Any:
+        assert 0 <= p < self._n
+
+        p += self._size
+        for i in range(self._log, 0, -1):
+            self._push(p >> i)
+        return self._d[p]
+
+    def prod(self, left: int, right: int) -> typing.Any:
+        assert 0 <= left <= right <= self._n
+
+        if left == right:
+            return self._e
+
+        left += self._size
+        right += self._size
+
+        for i in range(self._log, 0, -1):
+            if ((left >> i) << i) != left:
+                self._push(left >> i)
+            if ((right >> i) << i) != right:
+                self._push(right >> i)
+
+        sml = self._e
+        smr = self._e
+        while left < right:
+            if left & 1:
+                sml = self._op(sml, self._d[left])
+                left += 1
+            if right & 1:
+                right -= 1
+                smr = self._op(self._d[right], smr)
+            left >>= 1
+            right >>= 1
+
+        return self._op(sml, smr)
+
+    def all_prod(self) -> typing.Any:
+        return self._d[1]
+
+    def apply(self, left: int, right: typing.Optional[int] = None,
+              f: typing.Optional[typing.Any] = None) -> None:
+        assert f is not None
+
+        if right is None:
+            p = left
+            assert 0 <= left < self._n
+
+            p += self._size
+            for i in range(self._log, 0, -1):
+                self._push(p >> i)
+            self._d[p] = self._mapping(f, self._d[p])
+            for i in range(1, self._log + 1):
+                self._update(p >> i)
+        else:
+            assert 0 <= left <= right <= self._n
+            if left == right:
+                return
+
+            left += self._size
+            right += self._size
+
+            for i in range(self._log, 0, -1):
+                if ((left >> i) << i) != left:
+                    self._push(left >> i)
+                if ((right >> i) << i) != right:
+                    self._push((right - 1) >> i)
+
+            l2 = left
+            r2 = right
+            while left < right:
+                if left & 1:
+                    self._all_apply(left, f)
+                    left += 1
+                if right & 1:
+                    right -= 1
+                    self._all_apply(right, f)
+                left >>= 1
+                right >>= 1
+            left = l2
+            right = r2
+
+            for i in range(1, self._log + 1):
+                if ((left >> i) << i) != left:
+                    self._update(left >> i)
+                if ((right >> i) << i) != right:
+                    self._update((right - 1) >> i)
+
+    def max_right(
+            self, left: int, g: typing.Callable[[typing.Any], bool]) -> int:
+        assert 0 <= left <= self._n
+        assert g(self._e)
+
+        if left == self._n:
+            return self._n
+
+        left += self._size
+        for i in range(self._log, 0, -1):
+            self._push(left >> i)
+
+        sm = self._e
+        first = True
+        while first or (left & -left) != left:
+            first = False
+            while left % 2 == 0:
+                left >>= 1
+            if not g(self._op(sm, self._d[left])):
+                while left < self._size:
+                    self._push(left)
+                    left *= 2
+                    if g(self._op(sm, self._d[left])):
+                        sm = self._op(sm, self._d[left])
+                        left += 1
+                return left - self._size
+            sm = self._op(sm, self._d[left])
+            left += 1
+
+        return self._n
+
+    def min_left(self, right: int, g: typing.Any) -> int:
+        assert 0 <= right <= self._n
+        assert g(self._e)
+
+        if right == 0:
             return 0
-        if x <= l and r <= y:
-            return self.interval_tree[p]
-        self.give_lay_to_son(p, l, r)
-        mid = (l + r) // 2
-        s = 0
-        if x <= mid:
-            s += self.query(p * 2, l, mid, x, y)
-        if mid < y:
-            s += self.query(p * 2 + 1, mid + 1, r, x, y)
-        return s
 
-    def build_tree(self, p, l, r):
-        interval_tree = self.interval_tree
-        nums = self.nums
-        if l == r:
-            interval_tree[p] = nums[l - 1]
-            return
-        mid = (l + r) // 2
-        self.build_tree(p * 2, l, mid)
-        self.build_tree(p * 2 + 1, mid + 1, r)
-        interval_tree[p] = interval_tree[p * 2] + interval_tree[p * 2 + 1]
+        right += self._size
+        for i in range(self._log, 0, -1):
+            self._push((right - 1) >> i)
+
+        sm = self._e
+        first = True
+        while first or (right & -right) != right:
+            first = False
+            right -= 1
+            while right > 1 and right % 2:
+                right >>= 1
+            if not g(self._op(self._d[right], sm)):
+                while right < self._size:
+                    self._push(right)
+                    right = 2 * right + 1
+                    if g(self._op(self._d[right], sm)):
+                        sm = self._op(self._d[right], sm)
+                        right -= 1
+                return right + 1 - self._size
+            sm = self._op(self._d[right], sm)
+
+        return 0
+
+    def _update(self, k: int) -> None:
+        self._d[k] = self._op(self._d[2 * k], self._d[2 * k + 1])
+
+    def _all_apply(self, k: int, f: typing.Any) -> None:
+        self._d[k] = self._mapping(f, self._d[k])
+        if k < self._size:
+            self._lz[k] = self._composition(f, self._lz[k])
+
+    def _push(self, k: int) -> None:
+        self._all_apply(2 * k, self._lz[k])
+        self._all_apply(2 * k + 1, self._lz[k])
+        self._lz[k] = self._id
+
+
+"""例题
+CF242E
+"""
 
 
 #       ms
 def solve():
     n, = RI()
     a = RILST()
-    tree = [IntervalTree(n, [v >> i & 1 for v in a]) for i in range(20)]
     m, = RI()
+    op = lambda x, y: (x[0] + y[0], x[1] + y[1])  # 区间长度和1的个数都是求和合并
+    e = (0, 0)  # 区间长度，1的个数
+    mapping = lambda f, x: (x[0], x[0] - x[1]) if f else x  # 如果区间异或1则处理，否则不变
+    composition = lambda f, g: f ^ g  # 合并两次异或操作
+    id_ = 0  # 默认lazytag，对异或来说就是0
+    zkw = [ZKWLazy(op, e, mapping, composition, id_, [(1, v >> i & 1) for v in a]) for i in range(20)]
     for _ in range(m):
-        t, *q = RI()
+        t, *op = RI()
         if t == 1:
-            l, r = q
-            print(sum(tree[i].query(1, 1, n, l, r) << i for i in range(20)))
+            l, r = op
+            print(sum(zkw[i].prod(l - 1, r)[1] << i for i in range(20)))
         else:
-            l, r, x = q
+            l, r, x = op
             for i in range(20):
-                p = x >> i & 1
-                if p:
-                    tree[i].update(1, 1, n, l, r, 1)
+                if x >> i & 1:
+                    zkw[i].apply(l - 1, r, 1)
 
 
 if __name__ == '__main__':
-    n, = RI()
-    a = RILST()
-    tree = [IntervalTree(n, [v >> i & 1 for v in a]) for i in range(20)]
-    m, = RI()
-    for _ in range(m):
-        t, *q = RI()
-        if t == 1:
-            l, r = q
-            print(sum(tree[i].query(1, 1, n, l, r) << i for i in range(20)))
-        else:
-            l, r, x = q
-            for i in range(20):
-                p = x >> i & 1
-                if p:
-                    tree[i].update(1, 1, n, l, r, 1)
+    t = 0
+    if t:
+        t, = RI()
+        for _ in range(t):
+            solve()
+    else:
+        solve()
