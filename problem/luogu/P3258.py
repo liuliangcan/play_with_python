5f4a34d561ea491d36f2beb58914d207c3c07a53
liushuliang <liushuliang6@huawei.com>
2024-06-14 14:02:06 +0800

    洛谷 P3258.py 松鼠的新家 重链剖分+树上差分点差分板子题

---
+++ problem/luogu/P3258.py
@@ -0,0 +1,202 @@
+# Problem: P3258 [JLOI2014] 松鼠的新家
+# Contest: Luogu
+# URL: https://www.luogu.com.cn/problem/P3258
+# Memory Limit: 125 MB
+# Time Limit: 1000 ms
+
+import sys
+
+RI = lambda: map(int, sys.stdin.buffer.readline().split())
+RS = lambda: map(bytes.decode, sys.stdin.buffer.readline().strip().split())
+RILST = lambda: list(RI())
+DEBUG = lambda *x: sys.stderr.write(f'{str(x)}\n')
+
+
+
+
+class HLD:
+    def __init__(self, g, root):
+        # 无论是点还是dfn还是dep，都从1开始，默认0是无
+        n = len(g) - 1
+        self.g = g
+        self.fa = fa = [0] * (n + 1)  # 父节点，0表示无父节点
+        self.size = size = [1] * (n + 1)  # 子树大小
+        self.dep = dep = [0] * (n + 1)  # 深度，根深度为1
+        self.son = son = [0] * (n + 1)  # 重儿子，0表示无儿子
+        self.top = top = list(range(n + 1))  # 所在重链起点，起点就是自己
+        self.rank = rank = [0] * (n + 1)  # dfs序为i的节点编号
+        size[0] = 0
+        st = [root]
+        dep[root] = 1
+        tot = 1
+        while st:
+            u = st.pop()
+            rank[tot] = u  # 临时算一个非重儿子优先的dfn序，用于自底向上计算size
+            tot += 1
+            for v in g[u]:
+                if v == fa[u]: continue
+                fa[v] = u  # 父节点
+                dep[v] = dep[u] + 1  # 深度
+                st.append(v)
+        for u in rank[:0:-1]:  # 自底向上
+            for v in g[u]:
+                if v == fa[u]: continue
+                size[u] += size[v]
+                if size[v] > size[son[u]]: son[u] = v  # 更新重儿子
+
+        for u in rank[1:]:  # 自上而下更新链起点
+            for v in g[u]:
+                if v == son[u]: top[v] = top[u]
+        st = [root]
+        tot = 1
+        while st:  # 重新计算以重儿子优先的dfn序（可能有用）
+            u = st.pop()
+
+            rank[tot] = u
+            tot += 1
+            if son[u] == 0: continue  # 叶子
+            for v in g[u]:
+                if v != fa[u] and v != son[u]: st.append(v)
+            st.append(son[u])
+
+    def lca(self, u, v):  # 求u和v的最近公共祖先节点,复杂度lgn
+        fa = self.fa
+        dep = self.dep
+        top = self.top
+        while top[u] != top[v]:
+            if dep[top[u]] > dep[top[v]]:
+                u = fa[top[u]]
+            else:
+                v = fa[top[v]]
+        return v if dep[u] > dep[v] else u
+
+    def dis(self, u, v):
+        dep = self.dep
+        return dep[u] + dep[v] - 2 * dep[self.lca(u, v)]
+
+
+class HLD2:
+    def __init__(self, g, root):
+        # 无论是点还是dfn还是dep，都从1开始，默认0是无
+        n = len(g)
+        self.g = g
+        self.fa = [0] * (n + 5)  # 父节点，0表示无父节点
+        self.size = [1] * (n + 5)  # 子树大小
+        self.dep = [0] * (n + 5)  # 深度，根深度为1
+        self.son = [0] * (n + 5)  # 重儿子，0表示无儿子
+        self.top = [0] * (n + 5)  # 所在重链起点，起点就是自己
+
+        fa = self.fa;
+        size = self.size;
+        dep = self.dep;
+        son = self.son
+        top = self.top;
+        stk = [[root, 0, 0]]  # node,flag,fa
+        dep[root] = 1
+        while stk:
+            u, flag, father = stk.pop()
+            if flag:
+                for v in g[u]:
+                    if v != father:
+                        size[u] += size[v]
+                        if son[u] == 0 or size[v] > size[son[u]]:
+                            son[u] = v
+            else:
+                stk.append([u, 1, father])
+                for v in g[u]:
+                    if v != father:
+                        stk.append([v, 0, u])
+                        fa[v] = u
+                        dep[v] = dep[u] + 1
+        stk = [[root, root]]
+        tot = 1
+        while stk:
+            u, tops = stk.pop()
+            tot += 1
+            top[u] = tops
+            if son[u] == 0:
+                continue
+            for v in g[u]:
+                if v != fa[u] and v != son[u]:
+                    stk.append([v, v])
+            stk.append([son[u], tops])
+
+    def lca(self, u, v):  # 求u和v的最近公共祖先节点
+        fa = self.fa;
+        dep = self.dep;
+        top = self.top;
+        while top[u] != top[v]:
+            if dep[top[u]] > dep[top[v]]:
+                u = fa[top[u]]
+            else:
+                v = fa[top[v]]
+        return v if dep[u] > dep[v] else u
+
+    def dis(self, u, v):
+        dep = self.dep
+        return dep[u] + dep[v] - 2 * dep[self.lca(u, v)]
+class DiffOnTreePoint:
+    """点差分 u~v简单路上所有边权+=x, o=lca(u,v), p = parent[o]
+    diff[u]+=x,
+    diff[v]+=x,
+    diff[o]-=x,
+    diff[p]-=x;"""
+
+    def __init__(self, g, root=1, lca=None, a=None):  # 注意节点编号是1-indexed
+        self.n = n = len(g) - 1
+        self.lca = lca or HLD(g, root)  # 不传就重新算
+        self.diff = [0] * (n + 1)  # 树上差分,用0作为根的父节点
+        self.a = a if a else [0] * (n + 1)  # 传了就用它初始化；默认不修改，去掉切片则直接改
+        self.rank = self.lca.rank
+        self.fa = self.lca.fa  # 用0作为根的父节点
+        self.g = g
+
+    def add_route(self, u, v, w):
+        """把u~v简单路径上的所有点权+w"""
+        self.diff[u] += w
+        self.diff[v] += w
+        o = self.lca.lca(u, v)
+        self.diff[o] -= w
+        self.diff[self.fa[o]] -= w
+
+    def get_all_point_w(self):  #
+        d = self.diff[:]
+        for u in self.rank[:0:-1]:
+            for v in self.g[u]:
+                if v == self.fa[u]: continue
+                d[u] += d[v]
+            self.a[u] += d[u]
+
+        return self.a  # self.a[u]表示u的点权
+
+
+#       ms
+def solve():
+    n, = RI()
+    a = RILST()
+    g = [[] for _ in range(n + 1)]
+    for _ in range(n - 1):
+        u, v = RI()
+        g[u].append(v)
+        g[v].append(u)
+    s = [0] * (n + 1)
+    diff = DiffOnTreePoint(g, a[0], a=s)
+    for i in range(1, n):
+        diff.add_route(a[i - 1], a[i], 1)
+        s[a[i - 1]] -= 1
+
+    ans = diff.get_all_point_w()
+
+    ans[a[0]] += 1
+    ans[a[-1]] -= 1
+    print(*ans[1:], sep='\n')
+
+
+if __name__ == '__main__':
+    t = 0
+    if t:
+        t, = RI()
+        for _ in range(t):
+            solve()
+    else:
+        solve()
