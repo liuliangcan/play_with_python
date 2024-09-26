14099bdfd6d8590f5007b32ffb6990d3c9164e54
我本人 <liushuliang6@huawei.com>
2024-09-26 15:36:25 +0800

    前缀和+二分

--- problem/orac2/p0001.py
+++ problem/orac2/p0001.py
@@ -7,8 +7,40 @@ from sortedcontainers import SortedList
 RI = lambda: map(int, sys.stdin.buffer.readline().split())
 RS = lambda: map(bytes.decode, sys.stdin.buffer.readline().strip().split())
 RILST = lambda: list(RI())
+"""第k大的子段和，保证a[i]>=0
+二分+滑窗 nlogn
 
+solve1如果不保证a[i]>=0,用前缀和+sl，复杂度nlognlogSUM
+"""
 def solve():
+    n, = RI()
+    k, = RI()
+    a = []
+    for _ in range(n):
+        v, = RI()
+        a.append(v)
+
+    def ok(x):
+        l = 0
+        s = 0
+        cnt = 0
+        for r, v in enumerate(a):
+            s += v
+            while s >= x:
+                s -= a[l]
+                l += 1
+            cnt += l
+        return cnt < k
+    l, r = 0, sum(a)+1
+    while l+1<r:
+        mid = (l+r)>>1
+        if ok(mid):
+            r = mid
+        else:
+            l = mid
+    print(l)
+
+def solve1():
     n, = RI()
     k, = RI()
     a = []
